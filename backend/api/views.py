c2323a6590e2fbfb9c4cfdf07429228e03572193
inder782 <inderchandel782@gmail.com>
2023-11-29 16:37:47 +0530

    minor changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from .models import Video
 from rest_framework import status
 from rest_framework.response import Response
+from django.http import HttpResponse
 # Create your views here.
 
 @api_view(['POST'])
@@ -10,3 +11,4 @@ def Videohandle(request):
     if request.method=="POST":
         Video.save()
     return Response(status.is_success)
+
