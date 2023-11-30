58714c47d68314ae26600f7d52ecb3b981752578
inder782 <inderchandel782@gmail.com>
2023-11-30 17:32:17 +0530

    added the audio extractor functions

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
+    name=models.CharField(max_length=50,default=1)
     video_file=models.FileField(upload_to="vid/")
+    
+
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,18 +4,21 @@ from rest_framework import status
 from rest_framework.response import Response
 from .serializer import VideoSerializer
 from rest_framework.decorators import api_view
-
+from moviepy.editor import VideoFileClip
 # Create your views here.
 
 @csrf_exempt
 @api_view(["POST"])
 def Videohandle(request):
-    print(request.data)
     serializer=VideoSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        path="vid/How_Batman_even_manages_his_Sleep.mp4"
+        video_clip=VideoFileClip(path)
+        audio_clip=video_clip.audio
+        output="vid/audio/output_video.mp3"
+        audio_clip.write_audiofile(output)
         return Response(status=status.HTTP_200_OK)
     else:
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
 
