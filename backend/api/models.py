7c34b4264860e53120dd9a12e4576a0e4012b188
inder782 <inderchandel782@gmail.com>
2023-12-02 13:36:01 +0530

    added audio converter v1

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    name=models.CharField(max_length=50,default=1)
-    video_file=models.FileField(upload_to="vid/")
+
+    video_file=models.FileField(upload_to="output/vid/")
     
 
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,20 +5,36 @@ from rest_framework.response import Response
 from .serializer import VideoSerializer
 from rest_framework.decorators import api_view
 from moviepy.editor import VideoFileClip
-# Create your views here.
+from django.http import FileResponse, HttpResponse
+import os
 
 @csrf_exempt
 @api_view(["POST"])
 def Videohandle(request):
-    serializer=VideoSerializer(data=request.data)
+    name_data = str(request.FILES.get("video_file"))
+    serializer = VideoSerializer(data=request.data)
+
     if serializer.is_valid():
+        # save the video
         serializer.save()
-        path="vid/How_Batman_even_manages_his_Sleep.mp4"
-        video_clip=VideoFileClip(path)
-        audio_clip=video_clip.audio
-        output="vid/audio/output_video.mp3"
-        audio_clip.write_audiofile(output)
-        return Response(status=status.HTTP_200_OK)
+        
+        # extract the path to the video from the body
+        path = f"output/vid/{name_data.replace(' ', '_')}"
+        
+        # get the video clip
+        video_clip = VideoFileClip(path)
+        
+        # get the audio clip
+        audio_clip = video_clip.audio
+        
+        # extract and export the audio to the desired location
+        audio_output = f"output/audio/{name_data.replace(' ', '_')}"
+        audio_clip.write_audiofile(audio_output, codec="mp3") 
+
+        # create a FileResponse with the audio file
+        response = FileResponse(open(audio_output, 'rb'), content_type='audio/mp3')
+        response['Content-Disposition'] = f'attachment; filename="{name_data}"'
+        return response
+
     else:
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
